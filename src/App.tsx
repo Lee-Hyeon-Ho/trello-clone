73607e14187a82619fd26b159ec276066d22db60
이현호 <35526198+Lee-Hyeon-Ho@users.noreply.github.com>
2022-05-31 16:28:35 +0900

    update

---
+++ src/App.tsx
@@ -0,0 +1,71 @@
+import { DragDropContext, DropResult } from 'react-beautiful-dnd'
+import { useRecoilState } from 'recoil'
+import styled from 'styled-components'
+import { toDoState } from './atoms'
+import Board from './Components/Board'
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100vw;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const Boards = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  gap: 10px;
+`
+
+function App() {
+  const [toDos, setToDos] = useRecoilState(toDoState)
+  const onDragEnd = (info: DropResult) => {
+    const { destination, source } = info
+    if (!destination) return
+
+    if (destination?.droppableId === source.droppableId) {
+      setToDos((allBoards) => {
+        const boardCopy = [...allBoards[source.droppableId]]
+        const taskObj = boardCopy[source.index]
+        boardCopy.splice(source.index, 1)
+        boardCopy.splice(destination.index, 0, taskObj)
+        return {
+          ...allBoards,
+          [source.droppableId]: boardCopy,
+        }
+      })
+    }
+
+    if (destination.droppableId !== source.droppableId) {
+      setToDos((allBoards) => {
+        const sourceBoardCopy = [...allBoards[source.droppableId]]
+        const taskObj = sourceBoardCopy[source.index]
+        const destinationBoardCopy = [...allBoards[destination.droppableId]]
+        sourceBoardCopy.splice(source.index, 1)
+        destinationBoardCopy.splice(destination.index, 0, taskObj)
+        return {
+          ...allBoards,
+          [source.droppableId]: sourceBoardCopy,
+          [destination.droppableId]: destinationBoardCopy,
+        }
+      })
+    }
+  }
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Wrapper>
+        <Boards>
+          {Object.keys(toDos).map((boardId) => (
+            <Board key={boardId} boardId={boardId} toDos={toDos[boardId]} />
+          ))}
+        </Boards>
+      </Wrapper>
+    </DragDropContext>
+  )
+}
+
+export default App
